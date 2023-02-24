669000cfcc2911e4c25e498296d0d7f1602302ed
Qinwen Xu <suexu1025@gmail.com>
2023-02-24 23:54:22 +0000

    debug for size

--- benchmark_imagenet_ssd.py
+++ benchmark_imagenet_ssd.py
@@ -199,7 +199,7 @@ class Worker:
         num_batches = 0
         start = time.time()
         for j in range(10):
-            for batch in shard.iter_torch_batches(batch_size=256):
+            for batch in shard.iter_torch_batches(batch_size=1):
                 print(batch.shape)
                 batch = torch.as_tensor(batch['image'])
                 batch = xm.send_cpu_data_to_device(batch, device)
